313391e93fe8959e9bd8cd6aee689c0e81d5cd5b
Rodrigo Alcaraz de la Osa <rodri.alcaraz@gmail.com>
2023-11-09 20:22:08 +0100

    Update Hugo Blox

---
+++ update_hugo_blox.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Hugo Blox Updater
+# Helps update Hugo modules and the compatible Hugo version for Netlify.
+# https://docs.hugoblox.com/reference/update/
+#
+# Command: bash ./update_hugo_blox.sh
+
+# Check for prerequisites.
+if [ ! -d content ]; then
+  echo "ERROR: `cd` into your website folder before running this tool."
+  exit 1;
+fi
+
+# Update the Hugo Blox module
+function update_hugo_blox () {
+  # Update Hugo Blox to the latest main version
+  echo -e "Updating Hugo Modules to their latest version...\n"
+  hugo mod get -u ./...
+  hugo mod tidy
+}
+
+# Perform update
+update_hugo_blox
+
+echo
+echo "Warning: review the changes made by this helper script before pushing them to your site."
+echo
+echo "If there are breaking changes, the site structure, config, and/or front matter of content" \
+"may need upgrading by following the steps in the relevant consecutive release notes."
+echo
+echo "View the update guide at: https://docs.hugoblox.com/reference/update/"
+echo "View the latest release notes at: https://hugoblox.com/blog/"
